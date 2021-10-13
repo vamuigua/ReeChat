086ea8b9ce62ba667d46c5fda7de2da0d8a64118
Victor Allen <vamuigua@gmail.com>
2021-10-13 21:01:02 +0300

    feat(Login): added login composable
    Users can login to the system

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -14,22 +14,30 @@
 			autocomplete
 			v-model="password"
 		/>
+		<div class="error">{{ error }}</div>
 		<button>Log In</button>
 	</form>
 </template>
 
 <script>
 import { ref } from "@vue/reactivity";
+import useLogin from "../composables/useLogin";
+
 export default {
 	setup() {
 		const email = ref("");
 		const password = ref("");
 
-		const handleSubmit = () => {
-			console.log(email.value, password.value);
+		const { error, login } = useLogin();
+
+		const handleSubmit = async () => {
+			await login(email.value, password.value);
+			if (!error.value) {
+				console.log("User logged in");
+			}
 		};
 
-		return { email, password, handleSubmit };
+		return { email, password, handleSubmit, error };
 	},
 };
 </script>
