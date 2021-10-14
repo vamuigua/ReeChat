af21f93155a2da4b302b60a0dfae9a885e6e70e0
Victor Allen <vamuigua@gmail.com>
2021-10-14 11:21:00 +0300

    update(Navbar): added navbar to chatroom

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,9 +1,14 @@
 <template>
-	Chatroom
+	<div class="container">
+		<Navbar />
+	</div>
 </template>
 
 <script>
-export default {};
+import Navbar from "../components/Navbar";
+export default {
+	components: { Navbar },
+};
 </script>
 
 <style></style>
