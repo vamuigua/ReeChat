c4099c9def1cae802067a0e9346a4ddea4d6b928
Victor Allen <vamuigua@gmail.com>
2021-10-18 21:20:09 +0300

    feat(Authentication): added user signout
    Users can now logout from the application through Firebase signout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,12 +4,26 @@
 			<p>Hey there...display name here</p>
 			<p class="email">Currently logged in as... email</p>
 		</div>
-		<button>Logout</button>
+		<button @click="handleClick">Logout</button>
 	</nav>
 </template>
 
 <script>
-export default {};
+import useLogout from "../composables/useLogout";
+export default {
+	setup() {
+		const { logout, error } = useLogout();
+
+		const handleClick = async () => {
+			await logout();
+			if (!error.value) {
+				console.log("User Logged out");
+			}
+		};
+
+		return { handleClick };
+	},
+};
 </script>
 
 <style>
