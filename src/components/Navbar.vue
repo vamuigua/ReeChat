78d52cb1e5ac9d0871a3446b2642c2d1ef696458
Victor Allen <vamuigua@gmail.com>
2021-11-04 20:09:33 +0300

    update(Authentication): get current auth user
    App gets the currently logged in user details

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
-	<nav>
+	<nav v-if="user">
 		<div>
-			<p>Hey there...display name here</p>
-			<p class="email">Currently logged in as... email</p>
+			<p>Hey there {{ user.displayName }}</p>
+			<p class="email">Currently logged in as {{ user.email }}</p>
 		</div>
 		<button @click="handleClick">Logout</button>
 	</nav>
@@ -10,9 +10,12 @@
 
 <script>
 import useLogout from "../composables/useLogout";
+import getUser from "../composables/getUser";
+
 export default {
 	setup() {
 		const { logout, error } = useLogout();
+		const { user } = getUser();
 
 		const handleClick = async () => {
 			await logout();
@@ -21,7 +24,7 @@ export default {
 			}
 		};
 
-		return { handleClick };
+		return { handleClick, user };
 	},
 };
 </script>
