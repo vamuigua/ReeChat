da8ded27a545382e2ff99997d585254231d5bfd1
Victor Allen <vamuigua@gmail.com>
2021-11-10 20:08:37 +0300

    update(Auth): add route guard to chatroom

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Welcome from '../views/Welcome.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Welcome from "../views/Welcome.vue";
 import Chatroom from "../views/Chatroom.vue";
+import { projectAuth } from "../firebase/config";
+
+// auth guard
+const requireAuth = (to, from, next) => {
+	let user = projectAuth.currentUser;
+	console.log("Current user in auth guard: ", user);
+	if (!user) {
+		next({ name: "Welcome" });
+	} else {
+		next();
+	}
+};
 
 const routes = [
-  {
-    path: '/',
-    name: 'Welcome',
-    component: Welcome
-  },
-  {
-    path: '/chatroom',
-    name: 'Chatroom',
-    component: Chatroom
-  }
-]
+	{
+		path: "/",
+		name: "Welcome",
+		component: Welcome,
+	},
+	{
+		path: "/chatroom",
+		name: "Chatroom",
+		component: Chatroom,
+		beforeEnter: requireAuth,
+	},
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+	history: createWebHistory(process.env.BASE_URL),
+	routes,
+});
 
-export default router
+export default router;
